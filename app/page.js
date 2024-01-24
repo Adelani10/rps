1c2daa10600c8dd3dc46419301d67d32acec285d
ade <harounadelani@gmail.com>
2024-01-24 22:50:03 +0100

    changes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,33 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [playerPicked, setPlayerPicked] = useState(false);
+  const [score, setScore] = useState(0);
+  const [data, setData] = useState([
+    {
+      id: 2,
+      name: "paper",
+      image: "/icon-paper.svg",
+      borderColor: "purple",
+      picked: false,
+    },
+    {
+      id: 3,
+      name: "scissors",
+      image: "/icon-scissors.svg",
+      borderColor: "yellow",
+      picked: false,
+    },
+    {
+      id: 1,
+      name: "rock",
+      image: "/icon-rock.svg",
+      borderColor: "red",
+      picked: false,
+    },
+  ]);
+
   return (
     <main className="bg h-screen py-8 flex flex-col justify-between px-6">
       <div className=" container mx-auto space-y-16">
@@ -11,40 +38,62 @@ export default function Home() {
             <h2>scissors</h2>
           </div>
 
-          <div className="bg-[#f5f5f5] rounded-md w-1/4 flex flex-col items-center py-3 text-scoreText">
+          <div className="bg-[#f5f5f5] rounded-md w-1/4 md:w-1/6 flex flex-col items-center py-3 text-scoreText">
             <h3 className="uppercase text-xs tracking-wider">score</h3>
             <h3 className="uppercase text-3xl font-bold tracking-tighter">
-              12
+              {score}
             </h3>
           </div>
         </section>
 
-        <section className="flex flex-col space-y-8 items-center justify-center">
-          <div className="flex justify-center items-center space-x-12">
+        {!playerPicked ? (
+          <section className="flex flex-wrap max-w-[65%]  md:max-w-[40%] mx-auto gap-y-16 gap-x-28 items-center justify-center space-x-4 ">
+            {data.map((item) => {
+              return (
+                <button
+                key={item.id}
+                  onClick={() => setPlayerPicked(true)}
+                  className={`p-4 border ${item.borderColor === "purple" ? "bg-paperGradient1" : item.borderColor === "red" ? "bg-rockGradient1" : "bg-scissorsGradient1"} rounded-full`}
+                >
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="bg-white p-3 rounded-full w-24 h-24"
+                    id="paper"
+                  />
+                </button>
+              );
+            })}
+          </section>
+        ) : (
+          <div className="flex justify-center items-center space-x-8">
             <button className="p-2 bg-paperGradient2 rounded-full">
               <img
                 src="/icon-paper.svg"
                 alt="paper"
-                className="bg-white p-3 rounded-full w-16 h-16"
+                className="bg-white p-3 rounded-full w-24 h-24"
               />
             </button>
+
+            <div className="flex text-xl space-y-4 uppercase flex-col items-center font-bold">
+              <h2 className="text-red-600">You lost!</h2>
+              <button
+                onClick={() => setPlayerPicked(false)}
+                className="uppercase text-white border border-white px-4 rounded-md py-2 hover:bg-white hover:text-black"
+              >
+                play again
+              </button>
+            </div>
+
             <button className="p-2 bg-scissorsGradient2 rounded-full">
               <img
                 src="/icon-scissors.svg"
                 alt="paper"
-                className="bg-white p-3 rounded-full w-16 h-16"
+                className="bg-white p-3 rounded-full w-24 h-24"
               />
             </button>
           </div>
-
-          <button className="p-2 bg-rockGradient2 rounded-full">
-            <img
-              src="/icon-rock.svg"
-              alt="paper"
-              className="bg-white p-3 rounded-full w-16 h-16"
-            />
-          </button>
-        </section>
+        )}
       </div>
       <button className="uppercase text-white self-end border border-white px-4 rounded-md py-1 hover:bg-white hover:text-black">
         rules
